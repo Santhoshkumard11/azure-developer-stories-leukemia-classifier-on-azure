bde46daae590a599caa7f525768f8d4d9c0ba16b
Santhoshkumard11 <santhoshkdhana@gmail.com>
2021-04-01 16:04:52 +0530

    Updated the config file

--- leukemia-predict/cosmosdb_config.py
+++ leukemia-predict/cosmosdb_config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://leukemia-cosmosdb-eus.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'a3mp9H8rOtfoAlg1TBOlhPVVxfXpawMNQAr00A0aW4U3io0gtKPYUe4lksJEPazVPSP1PswAD893VsGJ7qO12Q=='),
+    'host': os.environ.get('ACCOUNT_HOST', 'https://leukemia-cosmosdbs-eus.documents.azure.com:443/'),
+    'master_key': os.environ.get('ACCOUNT_KEY', 'a3mp9H8rOtfoAlg1TBOlhPVVxfXpawMNQAr00A0aW4U3io0gtKPYUe4lksJEPazVPSP1PswAD893Vs1J7qO12Q=='),
     'database_id': os.environ.get('COSMOS_DATABASE', 'imagecontainer'),
     'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
 }
